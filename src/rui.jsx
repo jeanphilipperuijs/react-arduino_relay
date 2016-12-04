3a5f637fde38cc9f7b0b859d0c9c1b51197ba30f
Jean-Philippe Ruijs <jph@amapz.com>
2016-12-04 18:12:46 +0100

    build

--- src/rui.jsx
+++ src/rui.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-//import Header from 'rui/header';
-//import Footer from 'rui/footer';
-
 import ArduinoRelays from './relay.jsx';
-
-//ReactDOM.render( <Header />, document.getElementById( "header" ) );
-ReactDOM.render( <ArduinoRelays />, document.getElementById( "relay" ) );
-//ReactDOM.render( <Footer />, document.getElementById( "footerblock" ) );
+let relay = document.createElement("div");
+document.body.appendChild(relay);
+ReactDOM.render( <ArduinoRelays />, relay);
